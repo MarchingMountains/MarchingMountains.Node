8cc8f4489f270c70d0256861fcd539e021ba78be
IanFelton <ian@ianfelton.com>
2015-08-25 14:57:55 -0500

    Add tests and mocha

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-/*if (app.get('env') === 'development') {
+if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
@@ -73,17 +73,17 @@ app.use(function(req, res, next) {
             error: err
         });
     });
-}*/
+}
 
 // production error handler
 // no stacktraces leaked to user
-/*app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
-});*/
+});
 
 
 app.listen(app.get('port'), function() {
