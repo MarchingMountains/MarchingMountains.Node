2b4f888da9ae906b735fc052f7b5ba81ec9d6d30
IanFelton <ian@ianfelton.com>
2015-09-12 12:57:45 -0500

    cleanup authentication

--- server.js
+++ server.js
@@ -8,45 +8,8 @@ var express = require('express'),
 	routes = require('./app/routes'),
 	cookieParser = require('cookie-parser'),
 	methodOverride = require('method-override'),
-	session = require('express-session'),
-	passport = require('passport'),
-	Strategy = require('passport-local').Strategy;
+	session = require('express-session');
 var db = require('./db');
-
-//===============PASSPORT===============
-// Configure the local strategy for use by Passport.
-//
-// The local strategy require a `verify` function which receives the credentials
-// (`username` and `password`) submitted by the user.  The function must verify
-// that the password is correct and then invoke `cb` with a user object, which
-// will be set at `req.user` in route handlers after authentication.
-passport.use(new Strategy(
-  function(username, password, cb) {
-    db.users.findByUsername(username, function(err, user) {
-      if (err) { return cb(err); }
-      if (!user) { return cb(null, false); }
-      if (user.password != password) { return cb(null, false); }
-      return cb(null, user);
-    });
-  }));
-
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  The
-// typical implementation of this is as simple as supplying the user ID when
-// serializing, and querying the user record by ID from the database when
-// deserializing.
-passport.serializeUser(function(user, cb) {
-  cb(null, user.id);
-});
-
-passport.deserializeUser(function(id, cb) {
-  db.users.findById(id, function (err, user) {
-    if (err) { return cb(err); }
-    cb(null, user);
-  });
-});
 
 
 //===============EXPRESS================
@@ -61,26 +24,6 @@ app.use(require('cookie-parser')());
 app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.get('/login',
-  function(req, res){
-    res.render('login');
-  });
-  
-app.post('/login', 
-  passport.authenticate('local', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
-  
-app.get('/logout',
-  function(req, res){
-    req.logout();
-    res.redirect('/');
-  });
 
 app.use(function(req, res) {
   Router.run(routes, req.path, function(Handler) {
