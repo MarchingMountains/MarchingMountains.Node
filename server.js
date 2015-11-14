fac37b23b97739af832da60b43060f905fc903eb
IanFelton <ian@ianfelton.com>
2015-11-14 15:49:05 -0600

    Switch to koa

--- server.js
+++ server.js
@@ -1,66 +1,27 @@
-var express = require('express'),
-	path = require('path'),
-	logger = require('morgan'),
-	bodyParser = require('body-parser'),
+var	path = require('path'),
+	logger = require('koa-logger'),
+    serve = require('koa-static'),
 	swig  = require('swig'),
 	React = require('react'),
 	Router = require('react-router'),
 	routes = require('./app/routes');
+var koa = require('koa');
+var app = koa();
 
+app.use(logger());
 
-//===============EXPRESS================
-// Configure Express
- var app = express();
+// Serve static files
+app.use(serve(path.join(__dirname, 'public')));
 
-app.set('port', process.env.PORT || 3000);
-app.use(logger('dev'));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(function(req, res) {
-  Router.run(routes, req.path, function(Handler) {
-    var html = React.renderToString(React.createElement(Handler));
-    var page = swig.renderFile('views/index.html', { html: html });
-    res.send(page);
+app.use(function* () {
+    this.body = yield new Promise(resolve => {
+        Router.run(routes, this.req.url, function(Handler) {
+        var html = React.renderToString(React.createElement(Handler));
+        resolve(swig.renderFile('views/index.html', { html: html }));
+    });
   });
 }); 
 
-// mongoose
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-app.listen(app.get('port'), function() {
-	console.log('Express server listening on port ' + app.get('port')); 
+app.listen( 3000, function() {
+	console.log('Koa server listening on port ' +  3000); 
 });
