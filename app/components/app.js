15fc9f5ec776dc887b24d8980f0bf83a3d5969d4
IanFelton <ian@ianfelton.com>
2015-11-14 19:58:03 -0600

    force filesystem rename

--- app/components/app.js
+++ app/components/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {RouteHandler} from 'react-router';
-import Footer from './Footer';
-import Header from './Header';
+import Footer from './footer';
+import Header from './header';
 
 class App extends React.Component {
   render() {
