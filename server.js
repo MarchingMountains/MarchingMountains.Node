4bcf0889cebd3c3ca5342c85f5be03c5d6dbda87
IanFelton <ian@ianfelton.com>
2015-11-15 07:59:50 -0600

    Clean up files

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var	path = require('path'),
 	logger = require('koa-logger'),
     serve = require('koa-static'),
 	swig  = require('swig'),
-	React = require('react'),
-	Router = require('react-router'),
+	react = require('react'),
+	router = require('react-router'),
 	routes = require('./app/routes');
 var koa = require('koa');
 var app = koa();
@@ -31,8 +31,8 @@ app.use(serve(path.join(__dirname, 'public')));
 
 app.use(function* () {
     this.body = yield new Promise(resolve => {
-        Router.run(routes, this.req.url, function(Handler) {
-        var html = React.renderToString(React.createElement(Handler));
+        router.run(routes, this.req.url, function(handler) {
+        var html = react.renderToString(react.createElement(handler));
         resolve(swig.renderFile('views/index.html', { html: html }));
     });
   });
