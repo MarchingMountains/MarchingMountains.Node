0cad49587b23b7ae91500e867a178942d631a869
Katirene <kat.traxler@gmail.com>
2016-03-26 15:24:51 -0500

    committing changes

--- app.js
+++ app.js
@@ -2,14 +2,34 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+
 var schools = require('./server/routes/schools');
 var instruments = require('./server/routes/instruments');
+
+var index = require('./routes/index');
+var user = require('./routes/user');
+var register = require('./routes/register');
+// var connection = require('./server/modules/connection');
+
+var passport = require('./strategies/user_sql.js');
+var session = require('express-session');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(session({
+  secret: 'secret',
+  key: 'user',
+  resave: 'true',
+  saveUninitialized: false,
+  cookie: {maxage: 60000, secure: false}
+}));
+
 app.use('/schools', schools);
 app.use('/instruments', instruments);
+app.use('/register', register);
+app.use('/user', user);
+app.use('/', index);
 
 app.use(express.static('public'));
 app.use(express.static('public/views'));
