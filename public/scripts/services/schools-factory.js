8fababf52dce6307db7965b697e8e4ecbc2d8e92
Kailey Colestock <kjcolestock@gmail.com>
2016-03-29 19:47:06 -0500

    Chips functional, cleaned up comments

--- public/scripts/services/schools-factory.js
+++ public/scripts/services/schools-factory.js
@@ -2,7 +2,6 @@ myApp.factory('SchoolsFactory', ['$http', function($http) {
 
     var userID = 5;
 
-    //var factorySchoolsList = [];
     var factorySchoolsList = {};
     var schoolSearchResults = {};
     var selectedInstrumentName = {};
@@ -23,8 +22,6 @@ myApp.factory('SchoolsFactory', ['$http', function($http) {
 
     var factoryGetDirectorSchools = function() {
         var promise = $http.get('/schools/' + userID).then(function(response) {
-            //console.log('response: ', response);
-            //factorySchoolsList = response.data;
             factorySchoolsList.list = response.data;
         });
         return promise;
@@ -32,7 +29,6 @@ myApp.factory('SchoolsFactory', ['$http', function($http) {
 
     var factoryPostDirectorSchools = function(school) {
         var promise = $http.post('/schools/' + userID, school).then(function(response) {
-            //console.log('response: ', response);
             factoryGetDirectorSchools();
             factorySchoolsList.list = response.data;
         });
@@ -41,7 +37,6 @@ myApp.factory('SchoolsFactory', ['$http', function($http) {
 
     var factoryPutDirectorSchools = function(school) {
         var promise = $http.put('/schools/' + userID, school).then(function(response) {
-            //console.log('response: ', response);
             factoryGetDirectorSchools();
             factorySchoolsList.list = response.data;
         });
@@ -72,7 +67,6 @@ myApp.factory('SchoolsFactory', ['$http', function($http) {
         selectedSchoolInfo: selectedSchoolInfo,
         allSchools: factorySchoolsList,
         currentSchool: {}
-        //schoolsList: factorySchoolsList
     };
 
     return publicFunctions;
