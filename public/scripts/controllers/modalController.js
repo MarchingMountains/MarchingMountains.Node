4de1f6eea711378edb0d25fae36f218befc0ab0b
Kailey Colestock <kjcolestock@gmail.com>
2016-03-30 11:48:23 -0500

    Login Modal Functional, YAY

--- public/scripts/controllers/modalController.js
+++ public/scripts/controllers/modalController.js
@@ -1,21 +1,19 @@
-myApp.controller('ModalController', ['$scope','$location', 'UserService',
-    '$mdDialog', '$mdMedia', function($scope, $location, $mdDialog, $mdMedia, UserService) {
+myApp.controller('ModalController', ['$scope', '$mdDialog', '$mdMedia', 'UserService',
+    function($scope, $mdDialog, $mdMedia, UserService, LoginController) {
         console.log('inside Modal Controller');
 
         $scope.UserService = UserService;
 
         $scope.openModal = function(ev) {
-            //var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
+            var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
 
             $mdDialog.show({
                 templateUrl: '../views/templates/login.html',
                 controller: 'LoginController',
-                controllerAs: 'lc',
-                scope: $scope.$new(),
+                parent: angular.element(document.body),
                 targetEvent: ev,
-                clickOutsideToClose: true
+                clickOutsideToClose: true,
+                fullscreen: useFullScreen
                 });
             }
-
-
     }]);
