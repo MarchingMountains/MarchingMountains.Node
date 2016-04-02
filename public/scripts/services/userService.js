ea3f4a8aa3e6049cdf5167202cdeb9f13ea298f6
Katirene <kat.traxler@gmail.com>
2016-04-02 14:20:52 -0500

    log out completed in 2 separate functions

--- public/scripts/services/userService.js
+++ public/scripts/services/userService.js
@@ -16,32 +16,27 @@ myApp.factory('UserService', ['$http', '$window', function($http, $window) {
 
     function login(user) {
         console.log(user);
-            var promise = $http.post('/', user).then(function (response) {
-                console.log(response);
-                CurrentUser = new User (true, response.data.email, response.data.first_name, response.data.user_id);
-                console.log(CurrentUser);
-            });
+        var promise = $http.post('/', user).then(function (response) {
+            console.log(response);
+            CurrentUser = new User (true, response.data.email, response.data.first_name, response.data.user_id);
+            console.log(CurrentUser);
+        });
         return promise;
     }
 
     function register(user) {
         console.log(user);
-            var promise = $http.post('/register', user).then(function (response) {
-                console.log(response.data);
-                CurrentUser = new User (true, response.data.email, response.data.first_name, response.data.user_id);
-                console.log(CurrentUser);
-            });
+        var promise = $http.post('/register', user).then(function (response) {
+            console.log(response.data);
+            CurrentUser = new User (true, response.data.email, response.data.first_name, response.data.user_id);
+            console.log(CurrentUser);
+        });
         return promise;
     }
 
     function logOut() {
-        var promise = $http.get('/').then(function (response) {
-            if (response) {
-                CurrentUser = undefined;
-                $window.location.href = '/#/home';
-            }
-            });
-        return promise;
+        CurrentUser = undefined;
+        $window.location.href = '/#/home';
     }
 
     var publicFunctions = {
