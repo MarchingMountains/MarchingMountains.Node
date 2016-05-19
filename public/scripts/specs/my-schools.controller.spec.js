1e84db75c5a126eadb684489ed1aba660585440e
Ian Felton <ian@ianfelton.com>
2016-05-19 13:58:44 -0500

    start my-schools tests

---
+++ public/scripts/specs/my-schools.controller.spec.js
@@ -0,0 +1,138 @@
+describe('testing the MySchoolsController', function() {
+
+    var deferred;
+    var promise;   
+    var mockInstrumentsFactory;
+    var mockSchoolsFactory;
+    var mockDonationsFactory;
+    var mockUserService; 
+    
+    beforeEach(function() {
+        //'$scope', '$http',  'UserService'
+        module('myApp');
+
+        //Use bard to inject the adminFactory and a mock $http service
+        //'$scope', '$mdDialog', '$mdMedia', 'SchoolsFactory', 'InstrumentsFactory','DonationsFactory', 'UserService'
+        bard.inject(
+          'SchoolsFactory',
+          'DonationsFactory',
+          'InstrumentsFactory',
+          'UserService',
+          '$rootScope',
+          '$httpBackend',
+          '$controller',
+          '$q',
+          '$mdMedia',
+          '$mdDialog'
+        );
+
+        $scope = $rootScope.$new();
+        $httpBackend.when('GET', '/views/templates/home.html').respond({});
+        $httpBackend.when('GET', '../views/modals/add-school.html').respond({});
+        
+         //setup $q for testing promise in controller
+        deferred = $q.defer();
+        //get a promise instance
+        promise = deferred.promise;
+        mockUserService = {
+          askForCurrentUser: function() {
+            return {email: 'hi@here.com', first_name: 'Bob', user_id: '1', factoryUserId:10 };
+          },
+           watchCurrentUser : function() {
+                return CurrentUser;
+            },
+            logOutUser: function(){
+              return promise;
+            }
+        };
+
+        mockInstrumentsFactory = 
+         {
+            instruments :  {
+                  list:[{instrument:'Trumpet'},{instrument:'Clarinet'}]
+            },
+            factoryGetInstrumentsList: function() {
+               return promise;
+            },
+            putInstrument : function(instrument) {
+               return promise;
+            },
+            postInstrument : function(instrument) {
+               return promise;
+            },
+            deleteInstrument : function(instrument) {
+               return promise;
+            },
+            getStates : function(instrument) {
+               return promise;
+            }
+        };
+        mockSchoolsFactory = 
+         {
+            currentSchool : {
+                school_name:'testschool'
+            },
+            selectedSchoolInfo:{
+              list:[{'school':'school'}]
+            },
+            selectedInstrument:{
+              list:[{'instrument':'instrument'}]
+            },
+            getDirectorSchools: function(school){
+              return promise;
+            },
+            putDirectorSchool: function(school){
+              return promise;
+            },
+            schoolsList : function(){
+              return [{school:'school'}];
+            }
+        };
+      mockDonationsFactory = {
+        factoryGetCurrentUserDonations : function() {
+        return promise; 
+        },
+        factorySubmitDonation : function(donation) {
+        return promise; 
+        },
+        currentUserDonations: {donations: [{id:1}]}
+      }
+    });
+
+    it('should create my schools controller', function(done) {
+             //get an instance of donors controller and inject our mock services (we test services separately, so we don't care about testing services here, mocks are fine)
+        var controller = $controller('MySchoolsController', { $scope: $scope, UserService:mockUserService, DonationsFactory:mockDonationsFactory, SchoolsFactory:mockSchoolsFactory, InstrumentsFactory:mockInstrumentsFactory });
+        //apply scope to resolve all the promises
+        deferred.resolve();
+        $rootScope.$apply();
+        $httpBackend.flush();
+        expect(controller).to.not.be.undefined;
+      done();
+    });
+
+  // it('should add school controller', function(done) {
+  //            //get an instance of donors controller and inject our mock services (we test services separately, so we don't care about testing services here, mocks are fine)
+  //       var controller = $controller('MySchoolsController', { $scope: $scope, UserService:mockUserService, DonationsFactory:mockDonationsFactory, SchoolsFactory:mockSchoolsFactory, InstrumentsFactory:mockInstrumentsFactory });
+  //       $scope.addSchool({});
+  //       //apply scope to resolve all the promises
+  //       deferred.resolve();
+  //       $rootScope.$apply();
+  //       $httpBackend.flush();
+  //       expect(controller).to.not.be.undefined;
+  //     done();
+  //   });
+
+  //     it('should edit school controller', function(done) {
+  //            //get an instance of donors controller and inject our mock services (we test services separately, so we don't care about testing services here, mocks are fine)
+  //       var controller = $controller('MySchoolsController', { $scope: $scope, UserService:mockUserService, DonationsFactory:mockDonationsFactory, SchoolsFactory:mockSchoolsFactory, InstrumentsFactory:mockInstrumentsFactory });
+  //       $scope.editSchool({},{school:'school'});
+  //       //apply scope to resolve all the promises
+  //       deferred.resolve();
+  //       $rootScope.$apply();
+  //       $httpBackend.flush();
+  //       expect(controller).to.not.be.undefined;
+  //       expect(mockSchoolsFactory.currentSchool.school).to.equal('school');
+  //     done();
+  //   });
+});
+
